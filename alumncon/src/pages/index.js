b594f1c93c0b26279e20512562f558d015a12599
Nanditasunil <109030620+Nanditasunil@users.noreply.github.com>
2024-04-14 18:27:22 +0530

    first commit

---
+++ alumncon/src/pages/index.js
@@ -0,0 +1,7 @@
+import Login from "./Login";
+import Home from "./Home";
+import Profile from "./Profile";
+import ResetPassword from "./ResetPassword";
+import RegisterAc from "./RegisterAc";
+
+export { Login, Home, RegisterAc, Profile, ResetPassword };
